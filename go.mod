c4fc353e2275683ad202c7b703650a8d50d74ecb
Laptop <laptop@disroot.org>
2024-04-17 17:38:18 +0300

    first commit

---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/laptopcat/bs-fankit-rehosted
+
+go 1.21.3
+
+require (
+	github.com/json-iterator/go v1.1.12
+	github.com/valyala/fasthttp v1.52.0
+)
+
+require (
+	github.com/andybalholm/brotli v1.1.0 // indirect
+	github.com/klauspost/compress v1.17.6 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,158 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	jsoniter "github.com/json-iterator/go"
+	"github.com/valyala/fasthttp"
+)
+
+var json = jsoniter.ConfigFastest
+
+var client = fasthttp.Client{
+	MaxConnsPerHost: 99999,
+}
+
+var documents = map[string]string{
+	"game-assets": "324",
+	"logo":        "325",
+	"audio":       "373",
+}
+
+type ViewerData struct {
+	Asset struct {
+		Filename string `json:"filename"`
+	} `json:"asset"`
+}
+
+type SearchResult struct {
+	Token string `json:"token"`
+}
+
+type SearchData struct {
+	Data  []SearchResult `json:"data"`
+	Total int            `json:"total"`
+}
+
+func DownloadFile(token, document, base string) {
+	req := fasthttp.AcquireRequest()
+	defer fasthttp.ReleaseRequest(req)
+
+	req.SetRequestURI("https://fankit.supercell.com/api/viewer/data/" + token + "?document_id=" + document)
+	req.Header.Set("Accept-Encoding", "gzip, deflate, br")
+
+	resp := fasthttp.AcquireResponse()
+	defer fasthttp.ReleaseResponse(resp)
+
+	err := client.Do(req, resp)
+	if err != nil {
+		log.Println("error getting file viewer: ", err)
+		return
+	}
+
+	data, err := resp.BodyUncompressed()
+	if err != nil {
+		log.Println("error getting file viewer: ", err)
+		return
+	}
+
+	var vd ViewerData
+	err = json.Unmarshal(data, &vd)
+	if err != nil {
+		log.Println("error decoding file viewer: ", err, string(data))
+		return
+	}
+
+	req.SetRequestURI("https://brand.supercell.com/api/screen/download/" + token)
+
+	err = client.Do(req, resp)
+	if err != nil {
+		log.Println("error getting file url: ", err)
+		return
+	}
+
+	req.SetRequestURIBytes(resp.Header.Peek("location"))
+
+	err = client.Do(req, resp)
+	if err != nil {
+		log.Println("error downloading file: ", err)
+		return
+	}
+
+	data, err = resp.BodyUncompressed()
+	if err != nil {
+		log.Println("error downloading file: ", err)
+		return
+	}
+
+	os.WriteFile(base+vd.Asset.Filename, data, 0777)
+	log.Println("downloaded", vd.Asset.Filename)
+}
+
+func DownloadAll(folder, id string) {
+	os.Mkdir(folder, 0777)
+
+	req := fasthttp.AcquireRequest()
+	defer fasthttp.ReleaseRequest(req)
+
+	req.SetRequestURI("https://fankit.supercell.com/api/assets/search/" + id + "?limit=1")
+	req.Header.Set("Accept-Encoding", "gzip, deflate, br")
+
+	resp := fasthttp.AcquireResponse()
+	defer fasthttp.ReleaseResponse(resp)
+
+	err := client.Do(req, resp)
+	if err != nil {
+		log.Println("error getting file count for", folder, err)
+		return
+	}
+
+	data, err := resp.BodyUncompressed()
+	if err != nil {
+		log.Println("error getting file count for", folder, err)
+		return
+	}
+
+	var sd SearchData
+	err = json.Unmarshal(data, &sd)
+	if err != nil {
+		log.Println("error decoding file count for", folder, err, string(data))
+		return
+	}
+
+	log.Printf("Downloading %d files from %s\n", sd.Total, folder)
+	req.SetRequestURI(fmt.Sprintf("https://fankit.supercell.com/api/assets/search/%s?limit=%d", id, sd.Total))
+
+	err = client.Do(req, resp)
+	if err != nil {
+		log.Println("error getting files of", folder, err)
+		return
+	}
+
+	data, err = resp.BodyUncompressed()
+	if err != nil {
+		log.Println("error getting files of", folder, err)
+		return
+	}
+
+	err = json.Unmarshal(data, &sd)
+	if err != nil {
+		log.Println("error getting files of", folder, err, string(data))
+		return
+	}
+
+	folder += "/"
+	for _, file := range sd.Data {
+		DownloadFile(file.Token, id, folder) // don't spawn goroutine for higher stability, otherwise a lot of connections were failing
+	}
+}
+
+func main() {
+	for folder, id := range documents {
+		go DownloadAll(folder, id)
+	}
+
+	<-make(chan struct{})
+}
